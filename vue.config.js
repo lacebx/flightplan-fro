e3c8371747a90658fda3a34616be9056ecae0146
lacebx <arsenemanzi@pm.me>
2025-03-13 14:13:03 -0400

    edited vu.config

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/2025/flight-plan/t8/'
+    : '/',
   transpileDependencies: true,
   configureWebpack: {
     resolve: {
